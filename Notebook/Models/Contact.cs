7d8559ab2136e1ec70450c3ccbc6c719183bea0e
Алексей Шустров <geremusb@mail.ru>
2023-11-12 15:40:14 +0300

    Contact model

---
+++ Notebook/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Notebook.Models
+{
+    public class Contact
+    {
+        public string Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+    }
+}
